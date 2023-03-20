678c1c706fb57fa84cf39a1177add88818425a88
Röttger <moritzroettger@gmail.com>
2023-03-20 14:08:31 +0100

    update dep

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,4 @@
-console.log('test');
+console.log("test");
 
-var h1  = document.getElementById("title");
+var h1 = document.getElementById("title");
 console.log(h1);
-
-h1.style.color="white";
-h1.parentElement.style.backgroundColor="black"
